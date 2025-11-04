1de71b0f8e52b3e556c31e2cff83c1b0fdc32ab6
Benjamin Hanson <blhanson@ucsd.edu>
2025-11-04 13:33:17 -0800

    Changes

--- plot_corner_pdf.m
+++ plot_corner_pdf.m
@@ -1,178 +1,232 @@
-function plt = plot_corner_pdf(X,P,lbl,Xtruth,isovalue,p)
-%Plot Corner PDF
-%   PLT = PLOT_CORNER_PDF(X,P) takes the N-by-D coordinate matrix X and the
-%   D-by-1 probability vector P and creates a D-by-D corner figure. Each grid
-%   of the corner PDF contains a 2D contour plot with isovalues taken to be
-%   the 68-95-99.7% isocontours. 
-%
-%   PLT = PLOT_CORNER_PDF(X,P,LBL) takes the N-by-D coordinate matrix X 
-%   and the D-by-1 probability vector P and creates a D-by-D corner figure.
-%   Each grid of the corner PDF contains a 2D contour plot with isovalues 
-%   taken to be the 68-95-99.7% isocontours. The 1-by-D cell array LBL adds
-%   the axes lables to the left y-axes and the bottom x-axes.
-%
-%   PLT = PLOT_CORNER_PDF(X,P,LBL,XTRUTH) takes the N-by-D coordinate matrix 
-%   X  and the D-by-1 probability vector P and creates a D-by-D corner figure.
-%   Each grid of the corner PDF contains a 2D contour plot with isovalues 
-%   taken to be the 68-95-99.7% isocontours. The 1-by-D cell array LBL adds
-%   the axes lables to the left y-axes and the bottom x-axes. The D-by-1 truth 
-%   vector XTRUTH plots the true coordinate on each contour plot; on (i,j) 
-%   plots when i=j, this is represented as  a vline, and on (i,j) when i~=j
-%   this is represented as a scatter.
-%
-%   PLT = PLOT_CORNER_PDF(X,P,LBL,XTRUTH,ISOVALUE) takes the N-by-D coordinate 
-%   matrix X and the D-by-1 probability vector P and creates a D-by-D corner
-%   figure. Each grid of the corner PDF contains a 2D contour plot with 
-%   1-by-M ISOVALUE isocontours. The 1-by-D cell array LBL adds the axes 
-%   lables to the left y-axes and the bottom x-axes. The D-by-1 truth vector 
-%   XTRUTH plots the true coordinate on each contour plot. 
-%
-%   PLT = PLOT_CORNER_PDF(X,P,LBL,XTRUTH,ISOVALUE,p) takes the N-by-D 
-%   coordinate matrix X and the D-by-1 probability vector P and creates a 
-%   D-by-D corner figure. Each grid of the corner PDF contains a 2D contour 
-%   plot with 1-by-M ISOVALUE isocontours. The 1-by-D cell array LBL adds 
-%   the axes lables to the left y-axes and the bottom x-axes. The D-by-1 
-%   truth vector XTRUTH plots the true coordinate on each contour plot. 
-%   Each grid of the corner PDF contains a 2D contour plot with ISOVALUE 
-%   isocontours. p is used for plotting parameters. 
-%       * p -- plotting parameters (optional)
-%            *   color -- isosurface color
-%            * display -- handle visibility
-%            *    name -- display name, if display==1
-%            *   means -- plot weighted mean of point mass PDF
-%            *     axh -- figure axis
-%            *   alpha -- surface visibility
-%            *    type -- distribution type
-%
-%   Requirements: 
-%      - X must be cartesian gridded coordinates
-%      - plot_nongaussian_surface.m
-%
-%   Example: 
-%     figure; hold all; 
-%     d = 5; mu = randn(1,d); P = randn(d,d); P = P' * P; N = 20; x = zeros(d, N); 
-%     for i = 1:d, x(i,:) = linspace(-3 * sqrt(P(i,i)) + mu(i),3 * sqrt(P(i,i)) + mu(i),N); end
-%     [X1,X2,X3,X4,X5] = ndgrid(x(1,:)',x(2,:)',x(3,:)',x(4,:)',x(5,:)'); 
-%     X = [X1(:) X2(:) X3(:) X4(:) X5(:)]; 
-%     lbl = {"$x_1$", "$x_2$", "$x_3$", "$x_4$", "$x_5$"}; p.color = "jet"; 
-%     P = mvnpdf(X, mu, P); plot_corner_pdf(X,P,lbl,[],[], p);
+function plt = plot_corner_pdf(X,varargin)
+% plot_corner_pdf.m
+% Benjamin Hanson, 2025
 % 
-% Copyright 2025 by Benjamin L. Hanson, published under BSD 2-Clause License. 
+%   Given the N-by-D coordinate matrix X and the N-by-1 probability vector 
+%   P, creates a D-by-D corner figure. Each grid of the corner PDF contains 
+%   a 2D contour plot with isovalues taken to be the 68-95-99.7% isocontours.
+% 
+% Inputs:
+%          X -- N-by-D coordinate matrix
+%   varargin -- optional arguments
+%               * P -- weights of state vectors (optional)
+%               * p -- plotting parameters (optional)
+%                   *   color -- isosurface color
+%                   * display -- handle visibility
+%                   *    name -- display name, if display==1
+%                   *   means -- plot weighted mean of point mass PDF
+%                   *     axh -- figure axis
+%                   *   alpha -- surface visibility
+%                   *    type -- distribution type
+%                   *    hist -- plot histogram on i==j plots
+%               * aR -- alpha radius (for 2D and 3D functionality only)
+%               * isovalue -- isosurface value(s) to plot (for 2D and 3D functionality only)
+%               * axc -- figure axis to copy (because plotting alphaShape
+%               automatically changes the aspect ratio and limits of a plot
+%               * lbls -- labels of axes
+% 
+% Example:
+% 
+%   figure; hold on; 
+%   d = 5; mu = randn(1,d); P = randn(d,d); P = P' * P; N = 20; x = zeros(d, N); 
+%   for i = 1:d, x(i,:) = linspace(-3 * sqrt(P(i,i)) + mu(i),3 * sqrt(P(i,i)) + mu(i),N); end
+%   [X1,X2,X3,X4,X5] = ndgrid(x(1,:)',x(2,:)',x(3,:)',x(4,:)',x(5,:)'); 
+%   X = [X1(:) X2(:) X3(:) X4(:) X5(:)]; 
+%   lbl = {"$x_1$", "$x_2$", "$x_3$", "$x_4$", "$x_5$"}; p.color = "red"; p.type = "grid"; 
+%   P = mvnpdf(X, mu, P); plot_corner_pdf(X,'P',P,'lbls',lbl,'p',p);
 
-if nargin==2
-    [n, d] = size(X); 
-    if d < 2
-        error("BadXDimension");
-    end
-    [n_P, ~] = size(P);
-    if n_P ~= n
-        error("IncompatiblePSize");
-    end
-    lbl = {}; 
-    Xtruth = [];
-    isovalue = [normpdf(1)/normpdf(0), normpdf(2)/normpdf(0), normpdf(3)/normpdf(0)];
-elseif nargin==3
-    [n, d] = size(X); 
-    if d < 2
-        error("BadXDimension");
-    end
-    [n_P, ~] = size(P);
-    if n_P ~= n
-        error("IncompatiblePSize");
-    end
-    [~, d_lbl] = size(lbl); 
-    if d_lbl ~= d
-        error("IncompatibleLblSize");
-    end
-    Xtruth = [];
-    isovalue = [normpdf(1)/normpdf(0), normpdf(2)/normpdf(0), normpdf(3)/normpdf(0)];
-elseif nargin==4
-    [n, d] = size(X); 
-    if d < 2
-        error("BadXDimension");
-    end
-    [n_P, ~] = size(P);
-    if n_P ~= n
-        error("IncompatiblePSize");
-    end
-    [~, d_lbl] = size(lbl); 
-    if d_lbl ~= d
-        error("IncompatibleLblSize");
-    end
-    if ~isempty(Xtruth)
-        [d_t, ~] = size(Xtruth); 
-        if d_t ~= d
-            error("IncompatibleXtruthSize");
-        end
-    end
-    isovalue = [normpdf(1)/normpdf(0), normpdf(2)/normpdf(0), normpdf(3)/normpdf(0)];
-elseif nargin==5||nargin==6
-    [n, d] = size(X); 
-    if d < 2
-        error("BadXDimension");
-    end
-    [n_P, ~] = size(P);
-    if n_P ~= n
-        error("IncompatiblePSize");
-    end
-    [~, d_lbl] = size(lbl); 
-    if d_lbl ~= d
-        error("IncompatibleLblSize");
-    end
-    if ~isempty(Xtruth)
-        [d_t, ~] = size(Xtruth); 
-        if d_t ~= d
-            error("IncompatibleXtruthSize");
-        end
-    end
-    if ~isempty(isovalue)
-        [~, M] = size(isovalue);
-        if M < 3
-            error("MoreIsovaluesRequired");
-        end
+% variable arguments - defaults
+p.color = {"r", "r", "r"}; 
+p.display = 0; 
+p.means = 0;
+p.axh = gca; 
+p.alpha = [0.5, 0.3, 0.1];
+p.type = "curve";
+aR = [2 2 2]; 
+isovalue = [0.68, 0.95, 0.997]; 
+axc = p.axh; 
+scatter_b = 1; 
+
+for i=1:2:length(varargin)
+    if strcmp('P',varargin{i})
+        P = varargin{i+1};
+        scatter_b = 0; 
+    elseif strcmp('p',varargin{i})
+        p = varargin{i+1};
+    elseif strcmp('aR',varargin{i})
+        aR = varargin{i+1};
+    elseif strcmp('isovalue',varargin{i})
+        isovalue = varargin{i+1};
+    elseif strcmp('axc',varargin{i})
+        axc = varargin{i+1}; 
+    elseif strcmp('lbls',varargin{i})
+        lbls = varargin{i+1}; 
     else
-        isovalue = [normpdf(1)/normpdf(0), normpdf(2)/normpdf(0), normpdf(3)/normpdf(0)];
+        error(append("Unspecified argument: ", varargin{i}));
     end
-else
-    error("BadNumberOfInputs");
 end
 
-tiledlayout(d,d);
+
+[N, D] = size(X); 
+if scatter_b
+    P = zeros(N,1);
+end
+[NP, ~] = size(P); 
+
+% checks and balances
+if N~=NP
+    error("Incongruous state vector/weight sets.")
+end
+if ~isfield(p,'color')
+    for i = 1:numel(isovalue)
+        p.color{i}="r";
+    end
+else
+    if (isstring(p.color))||(ischar(p.color))||((all(size(p.color) == [1,3]))&&(~iscell(p.color)))
+        col = p.color; p.color = {}; 
+        for i = 1:numel(isovalue)
+            p.color{i}=col;
+        end
+    end 
+end
+if ~isfield(p,'display')
+    p.display=0;
+end
+if(p.display == 1)
+    if ~isfield(p,'name')
+        p.display=0;
+    end
+end
+if isfield(p,'name')
+    if ~isfield(p,'display')
+        p.display=1;
+    end
+end
+if ~isfield(p,'means')
+    p.means=0;
+end
+if ~isfield(p,'axh')
+    p.axh=gca;
+end
+if ~isfield(p,'alpha')
+    p.alpha=flip(logspace(log(0.5),log(0.75),numel(isovalue)));
+else
+    p.alpha = p.alpha.*ones(1,numel(isovalue));
+end
+if isfield(p,'type')
+    if strcmp(p.type, "curve")
+    elseif strcmp(p.type, "grid")
+    elseif strcmp(p.type, "scatter")
+        if isfield(p,'size')
+            if isscalar(p.size)
+                p.size = p.size.*ones(1,numel(isovalue));
+            end
+        else
+            p.size = 10.*ones(1,numel(isovalue));
+        end
+    else
+        error("Unsupported type.")
+    end
+else
+    p.type = "curve"; 
+end
+if scatter_b
+    if ~isfield(p,'marker')
+        p.marker='o';
+    end
+    if ~isfield(p,'ms')
+        p.ms=10;
+    end
+end
+if exist("aR", "var")
+    if isscalar(aR)
+        if aR < 0
+            error("alphaRadius may not be negative."); 
+        else
+            aR = aR.*ones(1,numel(isovalue));
+        end
+    else
+        if numel(aR) ~= numel(isovalue)
+            error("Incongruous alphaRadius/isovalue sets.");
+        else
+            if any(aR < 0)
+                error("alphaRadius may not be negative."); 
+            end
+        end
+    end
+end
+if (max(isovalue)>1)||(min(isovalue)<0)
+    error("Isovalue is outside of probability bounds [0,1].")
+end
+if ~isa(axc, 'matlab.graphics.axis.Axes')
+    error("Copy axis must be an axis variable.")
+end
+if ~exist("lbls", "var")
+    for i = 1:D
+        lbls{i} = "$x_" + num2str(i) + "$"; 
+    end
+else
+    [~, Dl] = size(lbls);
+    if Dl ~= D
+        error("Incompatible label array size.");
+    end
+end
+
+lbls_bool = 0;
+tl = findall(gcf,'Type','tiledlayout');
+if isempty(tl)
+    t = tiledlayout(D,D); % create only if none exists
+    t.TileSpacing = 'compact';
+    lbls_bool = 1; 
+else
+    % verify it matches D x D
+    if ~(tl.GridSize(1)==D && tl.GridSize(2)==D)
+        error("Existing tiledlayout has incompatible dimensions.");
+    end
+end
 
 count = 1; plt = {}; 
-for i = 1:d
+for i = 1:D
     for j = 1:i
-        nexttile((i-1)*d + j); hold on;
+        pause(0.1); nexttile((i - 1) * D + j); p.axh = gca; 
+        if lbls_bool
+            hold on; box on; set(p.axh, 'FontName', 'times', 'FontSize', 14);
+        end
         if i==j
-            if exist('p','var')
-                plt{count} = plot_nongaussian_surface(X(:,i),P,isovalue,p); 
+            if scatter_b
+                if p.hist
+                    plt{count} = histogram(X(:,i), 'Normalization', 'probability', 'FaceColor', p.color{1});
+                end
             else
-                plt{count} = plot_nongaussian_surface(X(:,i),P,isovalue); 
+                plt{count} = plot_nongaussian_surface(X(:,i),P,'p',p); 
             end
-            if ~isempty(Xtruth)
-                xline(Xtruth(i), "r-", "LineWidth", 2);
-            end
+            % xlim([min(X(:,i)), max(X(:,i))]);
         else
-            if exist('p','var')
-                plt{count} = plot_nongaussian_surface(X(:,[j,i]),P,isovalue,p); 
+            if scatter_b
+                plt{count} = scatter(X(:,j), X(:,i), p.ms, p.color{1}, 'filled', p.marker);
             else
-                plt{count} = plot_nongaussian_surface(X(:,[j,i]),P,isovalue); 
+                plt{count} = plot_nongaussian_surface(X(:,[j,i]),P,'p',p); 
             end
-            if ~isempty(Xtruth)
-                scatter(Xtruth(i), Xtruth(j), 50, "r", "filled", "Marker", "d");
+            % if ~lbls_bool
+            %     xlim([min(p.axh.XLim(1), min(X(:,j))), max(p.axh.XLim(2), max(X(:,j)))]);
+            %     ylim([min(p.axh.YLim(1), min(X(:,i))), max(p.axh.YLim(2), max(X(:,i)))]);
+            % else
+            %     xlim([min(X(:,j)), max(X(:,j))]);
+            %     ylim([min(X(:,i)), max(X(:,i))]);
+            % end
+        end
+        
+        if lbls_bool
+            if j == 1 && (i ~= 1)
+                ylabel(lbls{i}, 'FontSize', 18, 'FontName', 'Times', 'Interpreter', 'latex');
             end
+            if i==D
+                xlabel(lbls{j}, 'FontSize', 18, 'FontName', 'Times', 'Interpreter', 'latex');
+            end
+            if i ~= D, xticks([]); end
+            if (j ~= 1), yticks([]); end
         end
-
-        if j == 1
-            ylabel(lbl{i}, 'FontSize', 18, 'FontName', 'Times', 'Interpreter', 'latex');
-        elseif i ~= j
-            yticklabels([]);
-        end
-        if i==d
-            xlabel(lbl{j}, 'FontSize', 18, 'FontName', 'Times', 'Interpreter', 'latex');
-        else
-            xticklabels([]);
-        end
+        axis normal; count = count + 1; drawnow; 
     end
 end
 end
